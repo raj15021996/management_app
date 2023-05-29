be1a597a9cd7637fdcbf6dae30ccc07e8ccfc310
raj15021996 <rajlyf18@gmail.com>
2023-05-29 11:13:01 +0530

    style update

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import managementImg from '../assets/management.png'
 function Navbar() {
     return (
         <div className='h-14 bg-light-gray'>
-            <div className='flex text-white text-2xl h-full font-semibold items-center p-5'>
+            <div className='flex text-white text-2xl h-full font-semibold items-center p-6'>
                <img className='h-1/9' src={managementImg} alt='management'/> 
                <div>Management</div>
             </div>
